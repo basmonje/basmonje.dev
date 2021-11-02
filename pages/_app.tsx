c70cb2f4fcb13f869addef2b9917e52f9c555861
basmonje <basmonje@gmail.com>
2021-11-02 18:13:08 -0300

    add: styles scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from "next/app";
 
 import "@basmonje/quarks_styles";
 
+import "../styles/global.scss";
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
