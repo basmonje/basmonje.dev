cdb73f68fe7c7457352e650b21f9b6bef49162f4
basmonje <basmonje@gmail.com>
2022-06-10 14:23:13 -0400

    Fix page 404

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,16 +1,22 @@
 import NextLink from "next/link";
-import Container from "../src/components/Container";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import { enUS, esES } from "../lang";
+
+const Container = dynamic(import("components/Container"));
 
 export default function NotFound() {
+  const { locale } = useRouter();
+  const data = locale === "en-US" ? enUS : esES;
   return (
     <Container title="404 - Bastián Monje">
       <section className="px-5 my-13">
         <div className="container container-sm flex flex-col gap-2 px-5">
-          <h1>404 - No hemos encontrado lo que buscas.</h1>
+          <h1>{data.notFound.title}</h1>
           <div>
             <NextLink href="/">
               <a className="color-black-800 dark-color-white-800 outline-primary-400">
-                Volver al inicio
+                {data.notFound.button}
               </a>
             </NextLink>
           </div>
